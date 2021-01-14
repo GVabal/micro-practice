a4e96fa8c62f5216d4ab2e7dff1fcf605bec5f66
GVabal <gvabal@gmail.com>
2021-01-14 18:29:29 +0200

    change the way docker builds ui

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,3 +1,26 @@
 export const environment = {
   production: true
 };
+
+export const api = {
+  uri: {
+    postOffice: {
+      getAllParcels: 'http://localhost:8002/parcels',
+      receiveParcel: 'http://localhost:8002/parcels'
+    },
+    shop: {
+      getAllOrders: 'http://localhost:8000/orders',
+      takeOrder: 'http://localhost:8000/orders',
+      initSomeOrders: 'http://localhost:8000/orders/init'
+    },
+    warehouse: {
+      initSomeThings: 'http://localhost:8001/inventory/init',
+      getAllThings: 'http://localhost:8001/inventory',
+      doThing: 'http://localhost:8001/inventory/do'
+    },
+    factory: {
+      getAllOrders: 'http://localhost:8003/',
+      takeSingleThingOrder: 'http://localhost:8003/take-order'
+    }
+  }
+};
